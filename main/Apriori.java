f9e903921e3f580576b5fe6f013197945af135d9
Gabriel Gonzalez <gegb1994@gmail.com>
2019-10-11 00:32:06 -0400

    Changed input method

--- main/Apriori.java
+++ main/Apriori.java
@@ -1,37 +1,51 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Apriori{
 
     public static void main(String[] args) throws Exception{
 
-        File filePath = new File("C:\\Users\\gegb1\\IdeaProjects\\Apriori-Algorithm\\resources\\database_1.txt");
-        BufferedReader br = new BufferedReader(new FileReader(filePath));
-        Map<String, Integer> itemset = generateAllFrequentItemSets(br);
+        String file = new String("/Users/gegb1994/Documents/workspace/Apriori-Algorithm/resources/database_1.txt");
+        List<String> items = readFileInList(file);
+        Map<String, Integer> itemset = generateAllFrequentItemSets(items, 4);
         itemset.entrySet().stream()
                 .forEach(entry -> System.out.println(entry.getKey() + " " + entry.getValue()));
 
     }
 
-    public static Map<String, Integer> generateAllFrequentItemSets(BufferedReader file) throws Exception{
+    public static Map<String, Integer> generateAllFrequentItemSets(List<String> items, int support) throws Exception{
 
         Map<String, Integer> itemsMap = new HashMap<>();
-        String row;
-        while((row = file.readLine()) != null){
-            String[] transaction = row.split(",");
-            for(String st : transaction){
-                if(!itemsMap.containsKey(st.trim())){
-                    itemsMap.put(st.trim(), 1);
+        for(String st : items){
+            String[] transaction = st.split(",");
+            for(String t : transaction){
+                if(!itemsMap.containsKey(t.trim())){
+                    itemsMap.put(t.trim(), 1);
                 }
                 else {
-                    itemsMap.put(st.trim(), itemsMap.get(st.trim()) + 1);
+                    itemsMap.put(t.trim(), itemsMap.get(t.trim()) + 1);
                 }
             }
         }
 
+        itemsMap.entrySet().removeIf(entry -> entry.getValue().compareTo(support) < 0);
+
         return itemsMap;
+    }
+
+    public static List<String> readFileInList(String fileName)throws IOException {
+
+        List<String> lines = Collections.emptyList();
+        lines = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+        return lines;
+
     }
 
 }
